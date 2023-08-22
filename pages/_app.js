658b2ae03ec06ee543481dafd7fc462d0aeaa99d
David <doladepo128@gmail.com>
2023-08-22 02:42:54 +0100

    finished home screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import '../styles/globals.css';
 import { Layout } from '../components';
 import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast';
 
 
 export default function App({ Component, pageProps }) {
   return (
-     <Layout>
+    <StateContext>
+      <Toaster  />
+      <Layout>
        <Component {...pageProps} />
      </Layout>
+    </StateContext>
+     
   )
 }
